ef97ccc661702031c9860dfd3c6000a46b89b055
aadiisawant <adisawant0178@gmail.com>
2024-03-03 22:48:42 +0530

    fetch calls

--- SearchBar/src/components/SearchBar.jsx
+++ SearchBar/src/components/SearchBar.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {FaSearch} from 'react-icons/fa'
+import './SearchBar.css'
 function SearchBar() {
+  const [input, setInput] = useState("")
+  const fetchData = (value) => {
+    fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json())
+    .then((json) =>{
+    const results = json.filter((user)=> {
+      return value && user && user.name.toLowerCase().includes(value)
+    })
+    console.log(results);
+  })
+}
+  
+  
+  const handleChange = (value)=>{
+    setInput(value)
+    fetchData(value)
+  }
   return (
     <div className='input-wrapper'>
         <FaSearch id='search-icon'/>
-        <input placeholder='Type to search...' />
+        <input 
+        placeholder='Type to search...'
+        value={input}
+        onChange={(e)=> handleChange(e.target.value)} />
     </div>
          
   )
