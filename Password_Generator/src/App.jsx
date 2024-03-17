53c10fa82d776b15eb81f66efe5940f8d0c1d7ff
aadiisawant <adisawant0178@gmail.com>
2024-03-17 23:37:58 +0530

    minor changes

--- Password_Generator/src/App.jsx
+++ Password_Generator/src/App.jsx
@@ -1,16 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useState } from 'react'
+
 
 function App() {
   const [length, setLength] = useState(8)
   const [numAllowed, setNumAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  const passwordGenerator = useCallback(()=>{
+    let pass = ""
+    let str = "ASDFGHJKLZXCVBNMQWERTYUIOPqazxswedcvfrtgbnhyujmkiopl"
+    if(numAllowed) str += "0123456789"
+    if(charAllowed) str +="!@#$%^&*()_+{}[]~`_="
+    for(let i=1;i<=length;i++){
+      let char =Math.floor(Math.random()*str.length+1)
+      pass += str.charAt(char)
+      
+      console.log(pass);
+    }
+    setPassword(pass)
+  },[length,numAllowed,charAllowed,setPassword])
 
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-4 my-8 text-orange-500 bg-gray-700'>
+      <h1 className='text-center text-white my-3'>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'><input 
+        type="text"
+        value={password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='Password' 
+        readOnly
+        />
+        <button className='bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded outline-none'>Copy</button>
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex  items-center gap-x-1'>
+            <input type="range"
+            min={8}
+            max={15}
+            value={length}
+            className='cursor-pointer' 
+            onChange={(e)=> setLength(e.target.value)}/>
+            <label>Length: {length}</label>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
