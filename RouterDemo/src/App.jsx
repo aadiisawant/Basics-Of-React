5e27e935d7109407108df4acc66ca05edbc8236e
aadiisawant <adisawant0178@gmail.com>
2024-03-19 16:53:30 +0530

    router

---
+++ RouterDemo/src/App.jsx
@@ -0,0 +1,25 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import Blogs from './components/Blogs'
+import Contact from './components/Contact'
+import NoPage from './components/NoPage'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home/>} />
+          <Route path='blogs' element={<Blogs />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='*' element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>  
+  )
+}
+
+export default App
---
+++ RouterDemo/src/components/Blogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Blogs() {
+  return (
+    <div>
+      <h1>Hello, This Message is from Blogs Component.</h1>
+    </div>
+  )
+}
+
+export default Blogs
