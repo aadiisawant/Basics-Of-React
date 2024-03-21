c0a52851297868ab86d989e6e39d07fef639b1f7
aadiisawant <adisawant0178@gmail.com>
2024-03-21 15:21:24 +0530

    added RouterProvider to existing code

--- RouterDemo/src/App.jsx
+++ RouterDemo/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout'
 import Home from './components/Home'
@@ -8,17 +8,41 @@ import Contact from './components/Contact'
 import NoPage from './components/NoPage'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '',
+          element: <Home />
+        },
+        {
+          path: 'contact',
+          element: <Contact />
+        },
+        {
+          path: 'blogs',
+          element: <Blogs />
+        }
+      ]
+      
+    }
+  ])
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route index element={<Home/>} />
-          <Route path='blogs' element={<Blogs />} />
-          <Route path='contact' element={<Contact />} />
-          <Route path='*' element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>  
+    <>
+      <RouterProvider router={router}/>
+    </>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={<Layout/>}>
+    //       <Route index element={<Home/>} />
+    //       <Route path='blogs' element={<Blogs />} />
+    //       <Route path='contact' element={<Contact />} />
+    //       <Route path='*' element={<NoPage />} />
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>  
   )
 }
 
--- RouterDemo/src/components/Layout.jsx
+++ RouterDemo/src/components/Layout.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import Header from './Header'
+import Footer from './Footer'
 
 function Layout() {
   return (
     <div>
-      <nav>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/contact">Contact</Link>
-            </li>
-            <li>
-                <Link to="/blogs">Blogs</Link>
-            </li>
-        </ul>
-      </nav>
-      <Outlet/>
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
   )
 }
