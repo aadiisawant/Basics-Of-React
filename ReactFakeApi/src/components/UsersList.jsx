ed584096225bce70e698f8c533dc6f69495463d9
aadiisawant <adisawant0178@gmail.com>
2024-04-06 23:47:46 +0530

    minor changes

--- ReactFakeApi/src/components/UsersList.jsx
+++ ReactFakeApi/src/components/UsersList.jsx
@@ -6,13 +6,13 @@ function UsersList() {
   const [data, setData] = useState([]);
   const [isError, setIsError] = useState("");
   const url = 'https://reqres.in/api/users/2';
+  const url2 = 'https://reqres.in/api/users?page=2'
   useEffect(() => {
     
     const fetchData = async ()=>{
-      const result = await fetch(url)
+      const result = await fetch(url2)
       result.json().then(res =>{
-        setData(res)
-        console.log(res);
+        setData(res.data)
       })
 
     }
@@ -20,8 +20,11 @@ function UsersList() {
   },[])
   return (
   <>
-    <h1 key={data.data.id}>Name : {data.data.first_name} {data.data.last_name}</h1>
-    <h5>Email : {data.data.email}</h5>
+    {/* <h1 key={data.data.id}>Name : {data.data.first_name} {data.data.last_name}</h1>
+    <h5>Email : {data.data.email}</h5> */}
+    {data.map( (res)=>{
+      return  <li key={res.id}>{res.email}</li>
+    })}
   </>
   )
 }
