2c36421aff2c78d50447853732cfbb3dfeef4f91
aadiisawant <adisawant0178@gmail.com>
2024-04-07 12:58:38 +0530

    Async await and code optimized

--- axiosreact/src/App.jsx
+++ axiosreact/src/App.jsx
@@ -2,24 +2,40 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import axios from 'axios'
+import axios from './axios'
 
 function App() {
   const [myData , setMyData] = useState([])
   const [isError, setIsError] = useState("")
-  useEffect(()=>{
-    axios.get("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => setMyData(res.data))
-    .catch((e) =>{
-      setIsError(e.message)
-      console.log('Error : ',e);
-    })
-  },[]) 
+  const API = "https://jsonplaceholder.typicode.com"
+
+
+  // note: using axios to fetch details
+  // useEffect(()=>{
+  //   axios.get("https://jsonplaceholder.typicode.com/posts")
+  //   .then((res) => setMyData(res.data))
+  //   .catch((e) =>{
+  //     setIsError(e.message)
+  //     console.log('Error : ',e);
+  //   })
+  // },[]) 
     
+  // NOTE : Using Async Await
+  // using the base url from axios file (src/axios) 
+  const getAPIdata = async ()=>{
+    try{
+    const result =await axios.get("/posts")
+    setMyData(result.data)
+    }catch(e){
+      setIsError(e.message)
+    }
+  }
+  getAPIdata()
+
   return (
     <>
       {isError !=="" && <h2>{isError}</h2>}
-      {myData.map((data) => {
+      {myData.slice(0,12).map((data) => {
         const {id,title,body} = data
         return <div key={id}>
                   <h2>{title}</h2>
